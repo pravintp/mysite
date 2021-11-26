696da3424648d67b23761957cf530fd5d755c56c
LPravin <pravinvip77@gmail.com>
2021-11-26 09:05:00 +0530

    Refactor detail view by generic view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,8 @@
-from django.shortcuts import Http404, render
+from django.views import generic
 
 from .models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
