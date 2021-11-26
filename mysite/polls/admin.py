db56892ed4a77107c4ef9979c543f27d1b7bf382
LPravin <pravinvip77@gmail.com>
2021-11-26 10:45:32 +0530

    Add fieldset named Date Information fieldset

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,4 +6,13 @@ from .models import Choice, Question
 
 
 admin.site.register(Choice)
-admin.site.register(Question)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
