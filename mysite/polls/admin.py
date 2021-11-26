820ad9175faf04a9564d769f691a7b23a2c3b980
LPravin <pravinvip77@gmail.com>
2021-11-26 11:30:40 +0530

    Add was recently published header to question list view

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Question
+
 # Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date", "was_published_recently")
+
+
+admin.site.register(Question, QuestionAdmin)
