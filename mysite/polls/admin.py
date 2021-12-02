69a9fdca9604a09eb9f14125dbbcbb8897a71b32
pravintp <pravin@testpress.in>
2021-12-02 19:01:37 +0530

    Add search by question box

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,5 +5,21 @@ from .models import Choice, Question
 # Register your models here.
 
 
-admin.site.register(Choice)
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
