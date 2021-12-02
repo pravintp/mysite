912d73b4577bc7e3c95950b5f80a6d9c5b0db18f
pravintp <pravin@testpress.in>
2021-12-02 16:31:14 +0530

    Refactor result view using generic view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,6 +23,6 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
