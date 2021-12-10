4233533e48c1ac0182bc3135cfbb112390bb4b0b
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-10 15:26:05 +0530

    Create detail view for a question

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import Http404, render
 
 from .models import Question
-# Create your views here.
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
