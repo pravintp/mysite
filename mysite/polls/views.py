cfd1787e37f722e062e9caf8957ec193c0e85fc8
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-10 15:42:45 +0530

    Refactor index view using generic view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import HttpResponse, Http404, render
+from django.shortcuts import Http404, render, HttpResponse
+from django.views import generic
 
 from .models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 
 def detail(request, question_id):
