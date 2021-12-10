65ff3e64bbafe0e149b5de560903ec1cdac75e19
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-10 15:47:35 +0530

    Refactor result view using generic view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path(
+        "<int:question_id>/results/",
+        views.ResultsView.as_view(),
+        name="results",
+    ),
 ]
