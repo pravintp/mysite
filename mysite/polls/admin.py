19865802df64f632f79bc9985a41cd98ff6bc911
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-15 20:18:18 +0530

    Display choices inside respective question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,12 +5,18 @@ from .models import Choice, Question
 # Register your models here.
 
 
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+    inlines = [ChoiceInline]
 
 
-admin.site.register(Choice)
 admin.site.register(Question, QuestionAdmin)
