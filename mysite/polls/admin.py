c0913e29c5f60ded0ac96382f5c21036c0d3a1ef
pravintp <94511854+pravintp@users.noreply.github.com>
2021-12-15 20:29:29 +0530

    Add search by question box

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -18,7 +18,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_filter = ["pub_date"]
-
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
